62be8aed87fba5ea5e8f676b18841e5e2842744d
José Antonio Dongil Sánchez <jose.dongil@gmail.com>
2015-12-01 21:50:26 +0100

    [JDS] Ignore del fichero bundle de la app y quitar historico creado por react-router por defecto

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Router } from 'react-router';
 import routes from './config/routes';
+import createHistory from 'history/lib/createHashHistory';
 
-React.render(<Router routes={routes} />, document.body);
+const history = createHistory({
+  queryKey: false
+});
+
+React.render(<Router history={history} routes={routes} />, document.body);
